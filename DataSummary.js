2ebd77b46ca2be8c79ae7c7ef91a200d08dcc274
Kyle Nwosu <106222631+Komplete101@users.noreply.github.com>
2024-03-31 00:30:17 +0000

    Building llm

--- DataSummary.js
+++ DataSummary.js
@@ -1,12 +1,30 @@
-import fs from 'fs';
-import OpenAI from "openai";
+import { ChatOpenAI } from "@langchain/openai";
 
-const openai = new OpenAI();
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
-async function main(){
-    const file = await openai.files.create({
-        file: fs.createReadStream(""), //Will make a file upload
-        purpose: "data_summary"
-    });
-    console.log(file)
-}
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+
+
+import * as dotenv from "dotenv";
+dotenv.config();  // dont for get to add .env file with OPENAI_API_KEY
+
+const model = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo",
+    temperature: 0.5,
+});
+
+const prompt = ChatPromptTemplate.fromTemplate({
+    // This is the prompt that will be used to generate the response it will be made for a hospital visit summary on the doctors side 
+    //The question will be what is the summary of the appointment
+});
+
+const chain = await
+
+
+
